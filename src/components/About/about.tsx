e5b88aa5a1b67dc75df8315737677d2eb6eb44eb
walter lacoste <wlacoste@andreani.com>
2023-07-04 18:00:52 -0300

    cambio about

--- src/components/About/about.tsx
+++ src/components/About/about.tsx
@@ -8,9 +8,10 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 600,
   bgcolor: "background.paper",
-  border: "2px solid #000",
+  //   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  borderRadius: "var(--border-radius-large)",
 };
 
 export default function about() {
